8aac8bb7f980ae80ff81a847d23a642825694fa5
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-06-21 13:54:24 +0200

    feat: comment hamburger menu and add link for scroll

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -21,7 +21,7 @@
         </a>
       </div>
 
-      <div class="block lg:hidden pr-4">
+      <!-- <div class="block lg:hidden pr-4">
         <button
           id="nav-toggle"
           class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-teal-500 appearance-none focus:outline-none"
@@ -35,7 +35,7 @@
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
         </button>
-      </div>
+      </div> -->
 
       <div
         :class="{
@@ -46,35 +46,36 @@
         <ul class="list-reset lg:flex justify-end flex-1 items-center">
           <li class="mr-3">
             <a
-              class="inline-block py-2 px-4 text-black font-bold no-underline"
-              href="#"
-              >Active</a
+              class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
+              href="#teachers"
             >
+              Teachers
+            </a>
           </li>
           <li class="mr-3">
             <a
               class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-              href="#"
-              >link</a
+              href="#curriculum"
             >
+              Curriculum
+            </a>
           </li>
           <li class="mr-3">
             <a
               class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4"
-              href="#"
-              >link</a
+              href="#coc"
             >
+              Code of Conduct
+            </a>
           </li>
         </ul>
-        <button
+        <TicketButton
           :class="{
             'mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75': true,
             'gradient text-white': scrolled,
             'bg-white text-gray-800': !scrolled
           }"
-        >
-          Action
-        </button>
+        />
       </div>
     </div>
 
@@ -83,7 +84,11 @@
 </template>
 
 <script>
+import TicketButton from '@/components/eventbrite/TicketButton'
 export default {
+  components: {
+    TicketButton
+  },
   props: {
     scrolled: {
       type: Boolean,
