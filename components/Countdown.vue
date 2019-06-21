df56d25ea136ee8074f04b763e79f17fa7347f9f
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-06-21 13:53:24 +0200

    feat: countdown component

---
+++ components/Countdown.vue
@@ -0,0 +1,31 @@
+<template>
+  <no-ssr>
+    <vac :end-time="wDay">
+      <span slot="process" slot-scope="{ timeObj }">
+        <p class="text-5xl">{{ addLabel(timeObj.d, 'Day') }}</p>
+
+        <p class="text-4xl">{{ addLabel(timeObj.h, 'Hour') }}</p>
+        <p class="text-3xl">{{ addLabel(timeObj.m, 'Minutes') }}</p>
+        <p class="text-2xl">{{ addLabel(timeObj.s, 'Second') }}</p>
+      </span>
+    </vac>
+  </no-ssr>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      wDay: new Date(2019, 8, 27, 9).getTime()
+    }
+  },
+  methods: {
+    addLabel(value, unit) {
+      const result = `${value} ${unit}`
+      return +value === 1 ? result : result + 's'
+    }
+  }
+}
+</script>
+
+<style></style>
