585428745850046bdb9cf8993af0a2f8d8445e43
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-06-21 13:54:54 +0200

    chore: remove css classes fro ticket button

--- components/eventbrite/TicketButton.vue
+++ components/eventbrite/TicketButton.vue
@@ -1,6 +1,5 @@
 <template>
   <a
-    class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
     href="https://www.eventbrite.com/e/vueandme-codecamp-vue-wine-edition-tickets-63770322752?ref=ebtnebtckt"
     target="_blank"
   >
