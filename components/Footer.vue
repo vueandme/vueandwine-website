52de1433a7ea267e47f22609cfc9241470032a27
Nicolò Maria Mezzopera <nico@pulilab.com>
2019-06-21 13:53:36 +0200

    feat: foot component

---
+++ components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="bg-white">
+    <div class="container mx-auto  px-8">
+      <div class="w-full flex flex-row py-6 text-gray-800 items-end">
+        <div class="flex-1">
+          Have any questions? Reach out to us at:
+          <a href="mailto:hello@vueand.me">hello@vueand.me</a>
+        </div>
+        <div class="flex-1 text-right">
+          <a target="_blank" href="https://undraw.co">
+            Illustrations from unDraw
+          </a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
