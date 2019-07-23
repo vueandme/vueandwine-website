0e891a92f4369b62789edfbef6d17b6aa2610b65
gustojs <gustojs@protonmail.com>
2019-07-23 11:00:56 +0200

    Add logo, rewrite copy to Budapest edition, add Beginner workshop

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,23 +1,26 @@
 <template>
-  <div class="pt-24">
+  <div class="pt-24 bg-light-green">
     <div
-      class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center"
+      class="container px-3 max-w-5xl mx-auto flex flex-wrap flex-col md:flex-row items-center"
     >
-      <!--Left Col-->
+      <!--Right Col-->
       <div
-        class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left"
+        class="flex flex-col w-full md:w-3/5 justify-center items-start sm:text-center md:text-left ml-auto"
       >
-        <h1 class="my-4 text-6xl font-bold leading-tight">
+        <h1
+          class="my-4 text-6xl font-bold leading-tight text-dark-green sm:mx-auto md:mx-0"
+        >
           <slot name="main" />
         </h1>
-        <p class="leading-normal text-2xl mb-8">
+        <p
+          class="leading-normal text-2xl mb-8 text-dark-green sm:mx-auto md:mx-0"
+        >
           <slot name="sub" />
         </p>
-
         <slot name="action"> </slot>
       </div>
-      <!--Right Col-->
-      <div class="w-full md:w-3/5 py-6 text-center self-start">
+      <!--Left Col-->
+      <div class="w-full md:w-2/5 py-6 text-center self-start mr-auto">
         <slot name="right" />
       </div>
     </div>
