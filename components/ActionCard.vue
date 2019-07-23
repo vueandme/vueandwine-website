084a9bf538992d1da8c199f14ed5f844810aea21
gustojs <gustojs@protonmail.com>
2019-07-23 11:34:59 +0200

    Remove link

--- components/ActionCard.vue
+++ components/ActionCard.vue
@@ -3,14 +3,12 @@
     <div
       class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow"
     >
-      <a href="#" class="flex flex-wrap no-underline hover:no-underline">
-        <div class="w-full font-bold text-xl text-gray-800 px-6 pt-6">
-          <slot name="title"></slot>
-        </div>
-        <p class="text-gray-800 text-base px-6 mb-5 mt-5">
-          <slot />
-        </p>
-      </a>
+      <div class="w-full font-bold text-xl text-gray-800 px-6 pt-6">
+        <slot name="title"></slot>
+      </div>
+      <p class="text-gray-800 text-base px-6 mb-5 mt-5">
+        <slot />
+      </p>
     </div>
     <div
       class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6"
